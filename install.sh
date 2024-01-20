74558f928a2141498705797b2d1deb662ed21b56
magnus <pedmagnus@gmail.com>
2024-01-20 12:53:13 -0300

    new env

---
+++ install.sh
@@ -0,0 +1,3 @@
+ln -s /home/magnus/.dotfiles/alacritty /home/magnus/.config/alacritty
+ln -s /home/magnus/.dotfiles/wofi /home/magnus/.config/wofi
+ln -s /home/magnus/.dotfiles/sway /home/magnus/.config/sway
