98d0dcef92448cfd5c465582de45828e933994d3
Pedro Magnus <pedmagnus@gmail.com>
2024-01-31 15:02:59 -0300

    adding tmux to config files

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 ln -s /home/magnus/.dotfiles/alacritty /home/magnus/.config/alacritty
 ln -s /home/magnus/.dotfiles/wofi /home/magnus/.config/wofi
 ln -s /home/magnus/.dotfiles/sway /home/magnus/.config/sway
+ln -s /home/magnus/.dotfiles/tmux /home/magnus/.config/tmux
