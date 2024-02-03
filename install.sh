373a4498ca9c76c483ba865ebe89f86ecaaff12c
magnus <pedmagnus@gmail.com>
2024-02-03 13:11:17 -0300

    making install script better

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
-ln -s /home/magnus/.dotfiles/alacritty /home/magnus/.config/alacritty
-ln -s /home/magnus/.dotfiles/wofi /home/magnus/.config/wofi
-ln -s /home/magnus/.dotfiles/sway /home/magnus/.config/sway
-ln -s /home/magnus/.dotfiles/tmux /home/magnus/.config/tmux
+ALACRITTY_DIR="/home/$USER/.config/alacritty"
+TMP_DIR="~/.tmux/plugins/tmp"
+WOFI_DIR="/home/$USER/.config/wofi"
+SWAY_DIR="/home/$USER/.config/sway"
+TMUX_DIR="/home/$USER/.config/tmux"
+
+
+[ ! -d "$ALACRITTY_DIR" ] && ln -s /home/$USER/.dotfiles/alacritty /home/$USER/.config/alacritty
+[ ! -d "$WOFI_DIR" ] && ln -s /home/$USER/.dotfiles/wofi /home/$USER/.config/wofi
+[ ! -d "$SWAY_DIR" ] && ln -s /home/$USER/.dotfiles/sway /home/$USER/.config/sway
+[ ! -d "$TMUX_DIR" ] && ln -s /home/$USER/.dotfiles/tmux /home/$USER/.config/tmux
+
+
+[ ! -d "$TMP_DIR" ] &&  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
